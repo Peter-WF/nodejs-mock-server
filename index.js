3177c865f04354426ce5d0d607c59d7626ca928a
王斐 <wangfeia@zbj.com>
2017-11-02 02:40:27 +0800

    feat: finish express app proxy

--- index.js
+++ index.js
@@ -4,3 +4,29 @@
  * @date          2017-11-01 16:09
  * @description
  */
+
+const server = require('./server')
+
+// 设置全局变量 mock-server => MS
+const originMS = {}
+
+module.exports = {
+  init({ app, appDir }) {
+    // mock server proxy
+    global.MS = new Proxy(app, {
+      get(target, propKey) {
+        return originMS[propKey] != null ? originMS[propKey] : target[propKey]
+      }
+      // ,
+      // set(target, propKey, value) {
+      //   // 避免源示例对象
+      //   originMS[propKey] = value
+      // }
+    })
+
+    global.MS.appDir = appDir
+
+    // server init
+    server.init()
+  }
+}
